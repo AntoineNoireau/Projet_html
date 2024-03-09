00da5fc947c70ffa8d0888fb846d4eb5b0321b5b
AntoineNoireau <160619263+AntoineNoireau@users.noreply.github.com>
2024-03-09 16:45:57 +0000

    commit

--- script_2eme_jeu.js
+++ script_2eme_jeu.js
@@ -1,40 +1,39 @@
 let nombreColonnes;
 
-fetch("recettes-ingredient.json")
-    .then(response => response.json())
-    .then(data => {
+fetch("recettes-ingredients.json")
+  .then(response => response.json())
+  .then(data => {
+    const jsonRecettes = data;
 
-        const jsonRecettes = data;
-        
-        var min = 1;
-        var max = 2;
-        var numRecette = Math.floor(Math.random() * (max - min + 1)) + min;
-        console.log(numRecette);
-        nombreColonnes = jsonRecettes[2].data.filter(item => item.id_recette === numRecette).length;
+    var min = 1;
+    var max = 2;
+    var numRecette = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(numRecette);
+    nombreColonnes = jsonRecettes[2].data.filter(item => item.id_recette === numRecette).length;
 
-        console.log('Nombre de colonnes égal à', numRecette, ':', nombreColonnes);
+    console.log('Nombre de colonnes égal à', numRecette, ':', nombreColonnes);
 
-        const ingredientsIds = jsonRecettes[2].data
-            .filter(item => item.id_recette === numRecette)
-            .map(item => item.id_ingredient);
+    const ingredientsIds = jsonRecettes[2].data
+      .filter(item => item.id_recette === numRecette)
+      .map(item => item.id_ingredient);
 
-        fetch("ingredients.json")
-            .then(response => response.json())
-            .then(data => {
-                const ingredientsJson = data;
+    fetch("ingredients.json")
+      .then(response => response.json())
+      .then(data => {
+        const ingredientsJson = data;
 
-                ingredientsIds.forEach(ingredientId => {
-                    const ingredient = ingredientsJson.find(ingredient => ingredient.id === ingredientId);
-                    if (ingredient) {
-                        tableauHtml += '<td>' + ingredient.nom + '</td>';
-                    }
-                });
+        ingredientsIds.forEach(ingredientId => {
+          const ingredient = ingredientsJson.find(ingredient => ingredient.id === ingredientId);
+          if (ingredient) {
+            tableauHtml += '<td>' + ingredient.nom + '</td>';
+          }
+        });
 
-                tableauHtml += '</tr>';
-                tableauHtml += '</table>';
+        tableauHtml += '</tr>';
+        tableauHtml += '</table>';
 
-                document.getElementById('tableContainer').innerHTML = tableauHtml;
-            })
-            .catch(error => console.error('Erreur de chargement du fichier JSON "ingredients.json":', error));
-    })
-    .catch(error => console.error('Erreur de chargement du fichier JSON "recettes-ingredients.json":', error));
+        document.getElementById('tableContainer').innerHTML = tableauHtml;
+      })
+      .catch(error => console.error('Erreur de chargement du fichier JSON "ingredients.json":', error));
+  })
+  .catch(error => console.error('Erreur de chargement du fichier JSON "recettes-ingredients.json":', error));
