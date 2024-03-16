65a70819e5608fbb7446d942fc02c22d5ba9af30
Waysl0w <thocreusier@gmail.com>
2024-03-16 16:12:54 +0100

    Image amélioré mais pas complète

--- public/js/script_1er_jeu.js
+++ public/js/script_1er_jeu.js
@@ -3,23 +3,23 @@
 recette1 = [];
 recette2 = [];
 fetch("json/recettes-ingredients.json")
-    .then(response => response.json())
-    .then(data => {
+  .then(response => response.json())
+  .then(data => {
 
-        data[2].data.forEach(elem => {
-            // Accéder à chaque propriété (temp, vent, etc.)
-            if (elem.id_recette == 1) {
-                recette1.push(elem);
-            } else {
-                recette2.push(elem);
-            }
-        });
-
-
-
-
-
+    data[2].data.forEach(elem => {
+      // Accéder à chaque propriété (temp, vent, etc.)
+      if (elem.id_recette == 1) {
+        recette1.push(elem);
+      } else {
+        recette2.push(elem);
+      }
     });
+
+
+
+
+
+  });
 //On choppe les ingrédients ici
 /*
 ingredient = []
@@ -46,10 +46,11 @@ fetch("json/ingredients.json")
 const ingredientForm = document.getElementById("ingredientForm");
 let selectedIngredientsArray = []
 let Jeucontinu = true;
+let nbproposion = 0;
 
-ingredientForm.addEventListener("submit", function(event) {
-    
-  
+ingredientForm.addEventListener("submit", function (event) {
+
+
   event.preventDefault(); // Pour empêcher la soumission du formulaire
 
   const selectedIngredient = document.querySelector('input[name="ingredient"]:checked');
@@ -57,17 +58,23 @@ ingredientForm.addEventListener("submit", function(event) {
 
     selectedIngredientsArray.push(selectedIngredient.value);
     console.log(selectedIngredientsArray);
+    const resultatsDiv = document.getElementById("resultats");
+
+    const image = document.createElement("img");
+    image.src = "png/gris.png";
+    resultatsDiv.appendChild(image);
+
 
   } else {
     console.log("Aucun ingrédient sélectionné.");
   }
 
   if (selectedIngredientsArray.length === 5) {
-    console.log("Vérification");
+    
+            
     const verifArray = [0, 0, 0, 0, 0];
     for (let i = 0; i < selectedIngredientsArray.length; i++) {
-      console.log("Selected = " + selectedIngredientsArray[i] + "et recette[1] = " + recette1[i].id_ingredient);
-      if (selectedIngredientsArray[i] == recette1[i].id_ingredient) {
+       if (selectedIngredientsArray[i] == recette1[i].id_ingredient) {
         verifArray[i]++;
       } else {
         for (let j = 0; j < selectedIngredientsArray.length; j++) {
@@ -78,33 +85,71 @@ ingredientForm.addEventListener("submit", function(event) {
       }
     }
     selectedIngredientsArray = [];
-    console.log("Tableau reset");
 
 
     const resultatsDiv = document.getElementById("resultats");
-    verifArray.forEach((resultat) => {
-        const image = document.createElement("img");
-        if (resultat === 1) {
-          image.src = "png/vert.png"; // Image pour un ingrédient bien placé
-        } else if (resultat === 2) {
-            image.src = "png/orange.png"; // Image pour un ingrédient mal placé
-        } else {
-            image.src = "png/rouge.png"; // Image pour un ingrédient absent
-        }
-        resultatsDiv.appendChild(image); 
-      });
-
-      resultatsDiv.appendChild(document.createElement("br"));
-
-
+    
+    // Fonction pour remplacer progressivement les images grises par les images colorées
+    function remplacerImagesProgressivement(index) {
       
+      return new Promise(resolve => {
+        setTimeout(() => {
+          if (index < verifArray.length) {
+            
+            if(nbproposion==0)
+            {
 
+               image = resultatsDiv.children[index]; 
+            }
+            else
+            {
+
+               image = resultatsDiv.children[resultatsDiv.childElementCount - 6 +index];
+            }
+            // Sélectionne l'image existante
+            setTimeout(() => {
+              if (verifArray[index] === 1) {
+                image.src = "png/vert.png"; // Image pour un ingrédient bien placé
+              } else if (verifArray[index] === 2) {
+                image.src = "png/orange.png"; // Image pour un ingrédient mal placé
+              } else {
+                image.src = "png/rouge.png"; // Image pour un ingrédient absent
+              }
+            }, 50); // Délai avant de remplacer l'image grise par l'image colorée
+            remplacerImagesProgressivement(index + 1).then(resolve);
+          } else {
+            resolve();
+            
+          }
+        }, 1000); // Ajoute une pause d'une seconde entre chaque ajout
+      });
     }
 
-
+    // Début de la séquence de remplacement progressif
+    remplacerImagesProgressivement(0).then(() => {
 
       
+      
+      nbproposion ++;
+    });
+    resultatsDiv.appendChild(document.createElement("br"));
+    console.log(resultatsDiv)
     
     
+
+    
+
+
+
+
+
+
   }
+
+
+
+
+
+
+}
 );
