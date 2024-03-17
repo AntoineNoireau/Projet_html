72e8216ec05e153fc61a51a7532e03755ed92ee9
Kizakire <lol.vrack@gmail.com>
2024-03-17 20:15:08 +0100

    bdd commence à ête fonctionnel

--- app.js
+++ app.js
@@ -1,9 +1,55 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const connection = require('./connection');
 
 // Configurez Express pour servir les fichiers statiques du dossier 'public'
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/ingredients', (req, res) => {
+  const sqlQuery = 'SELECT * FROM ingredients'; // Adaptez cette ligne à votre requête SQL
+
+  connection.query(sqlQuery, (error, results) => {
+    if (error) {
+      console.error('Erreur lors de la requête :', error);
+      res.status(500).send('Erreur interne du serveur');
+      return;
+    }
+
+    // Supposons que vos résultats sont directement dans le format désiré
+    res.json(results);
+  });
+});
+
+app.get('/recettes-ingredients', (req, res) => {
+  const sqlQuery = 'SELECT * FROM recettesingredients'; // Adaptez cette ligne à votre requête SQL
+
+  connection.query(sqlQuery, (error, results) => {
+    if (error) {
+      console.error('Erreur lors de la requête :', error);
+      res.status(500).send('Erreur interne du serveur');
+      return;
+    }
+
+    // Supposons que vos résultats sont directement dans le format désiré
+    res.json(results);
+  });
+});
+
+app.get('/recettes', (req, res) => {
+  const sqlQuery = 'SELECT * FROM recettes'; // Adaptez cette ligne à votre requête SQL
+
+  connection.query(sqlQuery, (error, results) => {
+    if (error) {
+      console.error('Erreur lors de la requête :', error);
+      res.status(500).send('Erreur interne du serveur');
+      return;
+    }
+
+    // Supposons que vos résultats sont directement dans le format désiré
+    res.json(results);
+  });
+});
 
 // Démarrer le serveur
 const port = process.env.PORT || 3000;
--- public/js/script_2eme_jeu.js
+++ public/js/script_2eme_jeu.js
@@ -1,74 +1,61 @@
 let nombreColonnes;
 var numRecette
 ////////////////////affichage recette////////////////////////
-fetch("/json/recettes-ingredients.json")
+fetch("http://localhost:3000/ingredients")
   .then(response => response.json())
   .then(data => {
-    const jsonRecettes = data;
 
     var min = 1;
     var max = 2;
     numRecette = Math.floor(Math.random() * (max - min + 1)) + min;
-    nombreColonnes = jsonRecettes[2].data.filter(item => item.id_recette === numRecette).length;
+    nombreColonnes = data.filter(item => item.id_recette === numRecette).length;
 
     //console.log('Nombre de colonnes égal à', numRecette, ':', nombreColonnes);
 
-    const ingredientsIds = jsonRecettes[2].data
+    const ingredientsIds = data
       .filter(item => item.id_recette === numRecette)
       .map(item => item.id_ingredient);
 
-    fetch("/json/ingredients.json")
+    fetch("http://localhost:3000/ingredients")
       .then(response => response.json())
       .then(data => {
-        const ingredientsJson = data;
-
+        // 'data' est directement le tableau des ingrédients, pas besoin de '.data'
+        console.log(data);
         var tableauHtml = '<table> <tr>';
-
+    
         ingredientsIds.forEach(ingredientId => {
-          const ingredient = ingredientsJson[2].data.find(ingredient => ingredient.id === ingredientId);
-          //console.log(ingredient);
-          //console.log('id=' + ingredientId);
+          const ingredient = data.find(ingredient => ingredient.id === ingredientId); // Utilisez 'data.find' directement
           if (ingredient) {
-            //console.log(ingredient.nom);
-
             tableauHtml += '<td><img src="' + ingredient.image + '"></td>';
-            //console.log('tableauHtml=' + tableauHtml);
-
           }
         });
         tableauHtml += '</tr><tr>';
-
+    
         ingredientsIds.forEach(ingredientId => {
-          const ingredient = ingredientsJson[2].data.find(ingredient => ingredient.id === ingredientId);
-          //console.log(ingredient);
-          //console.log('id=' + ingredientId);
+          const ingredient = data.find(ingredient => ingredient.id === ingredientId); // Utilisez 'data.find' directement
           if (ingredient) {
-            //console.log(ingredient.nom);
-
             tableauHtml += '<td>' + ingredient.nom + '</td>';
-            //console.log('tableauHtml=' + tableauHtml);
-
           }
         });
-
+    
         tableauHtml += '</tr>';
         tableauHtml += '</table>';
-
+    
         document.getElementById('tableContainer').innerHTML = tableauHtml;
       })
-      .catch(error => console.error('Erreur', error));
+      .catch(error => console.error('Erreur:', error));
+    
   })
   .catch(error => console.error('Erreur', error));
 
 //////////////////////////Jeu///////////////////////////////////
 
-fetch("/json/recettes.json")
+fetch("http://localhost:3000/recettes")
   .then(response => response.json())
   .then(data => {
-    const jsonRecettes = data;
 
-    console.log(jsonRecettes[2].data);
-    var nomRecette = jsonRecettes[2].data.filter(item => item.id === numRecette)[0].nom;
+    console.log(data);
+    var nomRecette = data.filter(item => item.id === numRecette)[0].nom;
 
     let recetteChar = nomRecette.split('');
     let recetteCharSize = recetteChar.length;
