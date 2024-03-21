d3c8085cca38ff46c385209ca4211752499f2c8a
Kizakire <lol.vrack@gmail.com>
2024-03-21 15:17:46 +0100

    recodage du jeu 2

--- app.js
+++ app.js
@@ -51,6 +51,113 @@ app.get('/recettes', (req, res) => {
   });
 });
 
+app.get('/jeu_1', (req, res) => {
+  // Obtenir tous les ID de recette
+  connection.query('SELECT id FROM recettes', (error, recettes) => {
+    if (error) {
+      console.error('Erreur lors de la requête des recettes :', error);
+      res.status(500).send('Erreur interne du serveur');
+      return;
+    }
+
+    // Choisir un ID de recette aléatoire
+    const randomRecetteId = recettes[Math.floor(Math.random() * recettes.length)].id;
+
+    // Obtenir les informations de la recette aléatoire
+    connection.query(`SELECT * FROM recettes WHERE id = ${randomRecetteId}`, (error, randomRecette) => {
+      if (error) {
+        console.error('Erreur lors de la requête de la recette aléatoire :', error);
+        res.status(500).send('Erreur interne du serveur');
+        return;
+      }
+
+      // Obtenir les réponses de la recette aléatoire
+      connection.query(`SELECT * FROM recettesingredients WHERE id_recette = ${randomRecetteId}`, (error, ListRep) => {
+        if (error) {
+          console.error('Erreur lors de la requête des ingrédients de la recette :', error);
+          res.status(500).send('Erreur interne du serveur');
+          return;
+        }
+
+
+
+        // Ajouter 5 ingrédients aléatoires qui ne sont pas dans la recette
+        connection.query('SELECT * FROM ingredients WHERE id IN (SELECT id_ingredient FROM recettesingredients WHERE id_recette = ?)', [randomRecetteId], (error, ingredients) => {
+          if (error) {
+            console.error('Erreur lors de la requête des ingrédients :', error);
+            res.status(500).send('Erreur interne du serveur');
+            return;
+          }
+          connection.query('SELECT * FROM ingredients WHERE id NOT IN (SELECT id_ingredient FROM recettesingredients WHERE id_recette = ?) ORDER BY RAND() LIMIT 5', [randomRecetteId], (error, randomIngredients) => {
+            if (error) {
+              console.error('Erreur lors de la requête des ingrédients aléatoires :', error);
+              res.status(500).send('Erreur interne du serveur');
+              return;
+            }
+
+            // Envoyer la recette aléatoire et les ingrédients (y compris les aléatoires)
+            res.json({
+              recette: randomRecette,
+              listerep: ListRep,
+              ingredients: ingredients.concat(randomIngredients)
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+app.get('/jeu_2', (req, res) => {
+  // Obtenir tous les ID de recette
+  connection.query('SELECT id FROM recettes', (error, recettes) => {
+    if (error) {
+      console.error('Erreur lors de la requête des recettes :', error);
+      res.status(500).send('Erreur interne du serveur');
+      return;
+    }
+
+    // Choisir un ID de recette aléatoire
+    const randomRecetteId = recettes[Math.floor(Math.random() * recettes.length)].id;
+
+    // Obtenir les informations de la recette aléatoire
+    connection.query(`SELECT * FROM recettes WHERE id = ${randomRecetteId}`, (error, randomRecette) => {
+      if (error) {
+        console.error('Erreur lors de la requête de la recette aléatoire :', error);
+        res.status(500).send('Erreur interne du serveur');
+        return;
+      }
+
+      // Obtenir les réponses de la recette aléatoire
+      connection.query(`SELECT * FROM recettesingredients WHERE id_recette = ${randomRecetteId}`, (error, ListRep) => {
+        if (error) {
+          console.error('Erreur lors de la requête des ingrédients de la recette :', error);
+          res.status(500).send('Erreur interne du serveur');
+          return;
+        }
+
+
+
+        // Ajouter 5 ingrédients aléatoires qui ne sont pas dans la recette
+        connection.query('SELECT * FROM ingredients WHERE id IN (SELECT id_ingredient FROM recettesingredients WHERE id_recette = ?)', [randomRecetteId], (error, ingredients) => {
+          if (error) {
+            console.error('Erreur lors de la requête des ingrédients :', error);
+            res.status(500).send('Erreur interne du serveur');
+            return;
+          }
+
+          // Envoyer la recette aléatoire et les ingrédients (y compris les aléatoires)
+          res.json({
+            recette: randomRecette,
+            listerep: ListRep,
+            ingredients: ingredients
+          });
+        });
+      });
+    });
+  });
+});
+
 // Démarrer le serveur
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
