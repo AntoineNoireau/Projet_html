dd249af9879da94c4894907f5370a4949be4ae32
Kizakire <lol.vrack@gmail.com>
2024-03-22 08:35:22 +0100

    toto jeu travail sur bdd

--- public/js/script_1er_jeu.js
+++ public/js/script_1er_jeu.js
@@ -1,25 +1,52 @@
 
 //On choppe les recettes ici
-recette1 = [];
-recette2 = [];
-fetch("http://localhost:3000/recettes-ingredients")
+recette = [];
+ingredientdisplay = [];
+fetch("json/recettes-ingredients.json")
   .then(response => response.json())
   .then(data => {
 
-    data.forEach(elem => {
-      // Accéder à chaque propriété (temp, vent, etc.)
-      if (elem.id_recette == 1) {
-        recette1.push(elem);
-      } else {
-        recette2.push(elem);
+    const recettesIngredients = new Map();
+
+    data[2].data.forEach(elem => {
+      const recetteId = elem.id_recette;
+      const ingredientId = elem.id_ingredient;
+      if (!recettesIngredients.has(recetteId)) {
+        recettesIngredients.set(recetteId, []);
       }
+      recettesIngredients.get(recetteId).push(ingredientId);
+
     });
+    const randomNumber = Math.floor(Math.random() * 2) + 1;
+    console.log(randomNumber)
+    display = [];
+    recette = recettesIngredients.get(randomNumber);
+    
+    display = recette.slice();
+    const nonChoisies = Array.from(recettesIngredients.keys()).filter(id => id !== randomNumber)[0];
+    let randomIngredientIndex = 0;
+    console.log("Recette = " + recette);
+    for (let i = 0; i < 3; i++) {
+      randomIngredientIndex = Math.floor(Math.random() * recettesIngredients.get(nonChoisies).length);
+      while(display.includes(recettesIngredients.get(nonChoisies)[randomIngredientIndex]))
+      {
+        randomIngredientIndex = Math.floor(Math.random() * recettesIngredients.get(nonChoisies).length)
+      }
+      display.push(recettesIngredients.get(nonChoisies)[randomIngredientIndex])
 
-
+    }
+    
+    console.log(display);
+    console.log("Recette = " + recette);
+    for (let i = display.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [display[i], display[j]] = [display[j], display[i]]; // Échange des éléments
+    }
 
 
 
   });
+  
 //On choppe les ingrédients ici
 /*
 ingredient = []
@@ -40,9 +67,80 @@ fetch("json/ingredients.json")
   });
   */
 
+// Récupérer les données des ingrédients depuis le fichier JSON
+fetch("json/ingredients.json")
+  .then(response => response.json())
+  .then(data => {
+    const ingredients = data[2].data;
+
+    // Sélection de l'élément parent du formulaire
+    const ingredientFormParent = document.getElementById("ingredientForm");
+    console.log(ingredients)
+    console.log(display)
+
+    let name =ingredients.find(item => item.id === display[0]).nom;
+    console.log(name)
+    display.forEach(elem =>{
+      let nameelem = ingredients.find(item => item.id === elem).nom
+      const input = document.createElement("input");
+      input.type = "radio";
+      input.id = nameelem;
+      input.name = "ingredient";
+      input.value = elem; // Utilisation de l'ID comme valeur
+
+      const label = document.createElement("label");
+      label.htmlFor = nameelem;
+      label.textContent = nameelem;
+      ingredientFormParent.appendChild(input);
+      ingredientFormParent.appendChild(label);
+      ingredientFormParent.appendChild(document.createElement("br"));
 
 
-// on chope les ingrédients transmit par le form et quand y'en a 5
+
+
+      
+
+    })
+
+  /*
+    // Ajout des éléments au formulaire
+    ingredients.forEach(ingredient => {
+      const input = document.createElement("input");
+      input.type = "radio";
+      input.id = ingredient.nom; // Utilisation du nom comme ID
+      input.name = "ingredient";
+      input.value = ingredient.id; // Utilisation de l'ID comme valeur
+
+      const label = document.createElement("label");
+      label.htmlFor = ingredient.nom;
+      label.textContent = ingredient.nom;
+
+      ingredientFormParent.appendChild(input);
+      ingredientFormParent.appendChild(label);
+      ingredientFormParent.appendChild(document.createElement("br"));
+
+
+
+    });
+    */
+
+    // Ajout du bouton de soumission
+
+    const submitButton = document.createElement("button");
+    submitButton.id = "submitButton";
+    submitButton.textContent = "Valider";
+    ingredientFormParent.appendChild(submitButton);
+
+
+
+  })
+  .catch(error => console.error("Erreur lors de la récupération des données :", error));
+
+
+
+
+
+
 const ingredientForm = document.getElementById("ingredientForm");
 let selectedIngredientsArray = []
 let Jeucontinu = true;
@@ -67,20 +165,20 @@ ingredientForm.addEventListener("submit", function (event) {
     const icone = document.createElement("img");
     icone.src = "png/oeuf.png";
     icone.style.position = "absolute";
-    
+
     // Positionner l'icône au centre de l'image gris
     const grisRect = image.getBoundingClientRect();
     const iconeWidth = icone.offsetWidth;
     const iconeHeight = icone.offsetHeight;
     const iconeTop = grisRect.top + (grisRect.height - iconeHeight) / 2;
     const iconeLeft = grisRect.left + (grisRect.width - iconeWidth) / 2;
-    icone.style.marginTop = "-25px"; 
-    icone.style.marginLeft = "-25px"; 
-    
+    icone.style.marginTop = "-25px";
+    icone.style.marginLeft = "-25px";
+
     icone.style.top = iconeTop + "px";
     icone.style.left = iconeLeft + "px";
-    
-    icone.style.zIndex = "1"; 
+
+    icone.style.zIndex = "1";
     resultatsDiv.appendChild(icone);
 
 
@@ -89,48 +187,49 @@ ingredientForm.addEventListener("submit", function (event) {
   }
 
   if (selectedIngredientsArray.length === 5) {
-    
-            
+
+
     const verifArray = [0, 0, 0, 0, 0];
     for (let i = 0; i < selectedIngredientsArray.length; i++) {
-       if (selectedIngredientsArray[i] == recette1[i].id_ingredient) {
+      console.log("Ingédient select: " + selectedIngredientsArray[i]);
+
+      console.log("Ingédient bdd: " + recette[i]);
+      if (selectedIngredientsArray[i] == recette[i]) {
         verifArray[i]++;
       } else {
         for (let j = 0; j < selectedIngredientsArray.length; j++) {
-          if (selectedIngredientsArray[i] == recette1[j].id_ingredient && i != j) {
+          if (selectedIngredientsArray[i] == recette[j] && i != j) {
             verifArray[i] = 2;
           }
         }
       }
     }
     selectedIngredientsArray = [];
-    console.log("Verif = " +verifArray)
+    console.log("Verif = " + verifArray)
 
 
     const resultatsDiv = document.getElementById("resultats");
-    
+
     // Fonction pour remplacer progressivement les images grises par les images colorées
     function remplacerImagesProgressivement(index) {
-      
+
       return new Promise(resolve => {
         setTimeout(() => {
           if (index < 5) //car une div aura 10 element
           {
-            
-            if(nbproposion==0)
-            {
 
-               image = resultatsDiv.children[index*2]; 
+            if (nbproposion == 0) {
+
+              image = resultatsDiv.children[index * 2];
             }
-            else
-            {
+            else {
 
-               image = resultatsDiv.children[resultatsDiv.childElementCount - 11 +index*2];
-               //indice a modifié en fonction du nombre d'éléments dans la div
+              image = resultatsDiv.children[resultatsDiv.childElementCount - 11 + index * 2];
+              //indice a modifié en fonction du nombre d'éléments dans la div
             }
             // Sélectionne l'image existante
             setTimeout(() => {
-              console.log("verif[index] = "+verifArray[index*2])
+              console.log("verif[index] = " + verifArray[index ])
               if (verifArray[index] === 1) {
                 image.src = "png/vert.png"; // Image pour un ingrédient bien placé
               } else if (verifArray[index] === 2) {
@@ -139,10 +238,10 @@ ingredientForm.addEventListener("submit", function (event) {
                 image.src = "png/rouge.png"; // Image pour un ingrédient absent
               }
             }, 50); // Délai avant de remplacer l'image grise par l'image colorée
-            remplacerImagesProgressivement(index+1).then(resolve);
+            remplacerImagesProgressivement(index + 1).then(resolve);
           } else {
             resolve();
-            
+
           }
         }, 1000); // Ajoute une pause d'une seconde entre chaque ajout
       });
@@ -152,14 +251,14 @@ ingredientForm.addEventListener("submit", function (event) {
     remplacerImagesProgressivement(0).then(() => {
       console.log(resultatsDiv)
       console.log(resultatsDiv.children[0])
-      nbproposion ++;
+      nbproposion++;
     });
     resultatsDiv.appendChild(document.createElement("br"));
 
-    
-    
 
-    
+
+
+
 
 
 
