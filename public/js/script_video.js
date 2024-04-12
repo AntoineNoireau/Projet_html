7576796ad7251572c689b0a2b0b2b1a3d63676c9
AntoineNoireau <antoine.noireau@gm.com>
2024-04-12 11:04:23 +0200

    c

--- public/js/script_video.js
+++ public/js/script_video.js
@@ -103,7 +103,7 @@ function init() {
 
                 imgtitre = document.getElementById("texte" +videoId.toString());
                 imgtitre.textContent = titre;
-                ajusterHauteurFond(".video");
+                ajusterHauteurFond(".video_princ");
             })
 
     });
